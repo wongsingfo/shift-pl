1124efb1e79d214285f95d864eb4cf6102fcacad
Chengke <wongck@pku.edu.cn>
2020-05-22 20:53:56 +0800

    [run.sh] add error notification

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
-set -e  # die on error
+set -o pipefail
 
 make -s
+
+if [ $? -ne 0 ]; then
+	echo !!!! ERROR !!!!
+	echo failed to build the program
+	exit 1
+fi
 
 ./f $1 \
 	| awk 'NR % 5 == 4' \
 	| sed 's/$/;/'      \
 	| ./fullrecon/f /dev/stdin
+
+if [ $? -ne 0 ]; then
+	echo !!!! ERROR !!!!
+	echo An error occured during the processing
+	exit 1
+fi
